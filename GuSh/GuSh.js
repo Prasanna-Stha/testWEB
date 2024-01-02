012d297519b312881e3e4531d0263014ceb96543
Prasanna Shrestha <155393026+Prasanna-Stha@users.noreply.github.com>
2024-01-02 16:20:32 +0545

    Initial upload

---
+++ GuSh/GuSh.js
@@ -0,0 +1,5 @@
+let showNav=document.getElementsByClassName("Menu");
+showNav[0].addEventListener("click", function(){
+    let visibleNav=document.getElementById("nav-Menu");
+    visibleNav.style.visibility="visible";
+})
